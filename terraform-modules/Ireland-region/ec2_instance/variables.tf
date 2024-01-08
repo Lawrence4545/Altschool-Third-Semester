507bab620ecca735eaf98bbda82779704391f0d2
Lawrence4545 <oluwasegunadeniyi45@gmail.com>
2024-01-07 17:32:21 -0800

    first commit

---
+++ terraform-modules/Ireland-region/ec2_instance/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "region" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "ami" {}
+variable "key_pair" {}
+variable "type" {}
+
