12cdf9c0c227c2b3234a44bfc7d5e2fcf2a074d3
Lawrence4545 <oluwasegunadeniyi45@gmail.com>
2024-01-12 05:37:40 -0800

    Committing changes in submodule

--- Assignment-1/terraform-modules/Frankfurt-region/ec2_instance/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {}
-variable "project_name" {}
-variable "type" {}
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "ami" {}
-variable "key_pair" {}
-
--- Assignment-1/terraform-modules/Ireland-region/ec2_instance/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "region" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "ami" {}
-variable "key_pair" {}
-variable "type" {}
-
